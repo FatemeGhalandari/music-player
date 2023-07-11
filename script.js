9ef742e2f422804a542659b8220da16e91efd979
Fateme Ghalandari <fghalandarii@gmail.com>
2023-07-11 15:37:10 +0330

    add some eventListeners to buttons

--- script.js
+++ script.js
@@ -7,8 +7,10 @@ const image = document.querySelector("#image");
 const progress = document.querySelector(".progress");
 const progressContainer = document.querySelector(".progress-container");
 let title = document.getElementById("title");
-const song = "hey";
-loadSong(song);
+
+const songs = ["hey", "summer", "ukulele"];
+let songIndex = 2;
+loadSong(songs[songIndex]);
 
 function loadSong(song) {
   title.innerText = song;
@@ -18,14 +20,16 @@ function loadSong(song) {
 
 function pauseSong() {
   musicContainer.classList.remove("play");
-  playBtn.querySelector("i.fas").classList.remove("fa-play");
-  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  playBtn.querySelector("i.fas").classList.remove("fa-pause");
+  playBtn.querySelector("i.fas").classList.add("fa-play");
+  audio.pause();
 }
 
 function playSong() {
   musicContainer.classList.add("play");
-  playBtn.querySelector("i.fas").classList.remove("fa-pause");
-  playBtn.querySelector("i.fas").classList.add("fa-play");
+  playBtn.querySelector("i.fas").classList.remove("fa-play");
+  playBtn.querySelector("i.fas").classList.add("fa-pause");
+  audio.play();
 }
 
 playBtn.addEventListener("click", () => {
@@ -36,3 +40,40 @@ playBtn.addEventListener("click", () => {
     playSong();
   }
 });
+
+function prevSong() {
+  songIndex--;
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function nextSong() {
+  songIndex++;
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
+  loadSong(songs[songIndex]);
+  playSong();
+}
+
+function updateProgress(e) {
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
+}
+
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+  audio.currentTime = (clickX / width) * duration;
+}
+
+prevBtn.addEventListener("click", prevSong);
+nextBtn.addEventListener("click", nextSong);
+audio.addEventListener("timeupdate", updateProgress);
+progressContainer.addEventListener("click", setProgress);
+progress.addEventListener("ended", nextSong);
